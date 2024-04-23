6fc79e4ae19f63038b1882babbb6ffd29cae0994
brain-dev-null <git@braindevnull.space>
2024-04-23 21:20:35 +0200

    Add global reference objects for TRUE, FALSE and NULL

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -5,6 +5,12 @@ import (
 	"github.com/brain-dev-null/monkey-interpreter/object"
 )
 
+var (
+    NULL  = &object.Null{}
+    TRUE  = &object.Boolean{Value: true}
+    FALSE = &object.Boolean{Value: false}
+)
+
 
 func Eval(node ast.Node) object.Object {
     switch node := node.(type) {
@@ -16,7 +22,7 @@ func Eval(node ast.Node) object.Object {
     case *ast.IntegerLiteral:
         return &object.Integer{Value: node.Value}
     case *ast.Boolean:
-        return &object.Boolean{Value: node.Value}
+        return nativeBoolToBooleanObject(node.Value)
     }
 
 
@@ -31,4 +37,11 @@ func evalStatements(stmts []ast.Statement) object.Object {
     }
 
     return result
+}
+
+func nativeBoolToBooleanObject(input bool) *object.Boolean {
+    if input {
+        return TRUE
+    }
+    return FALSE
 }
