7b9a5e3f628463c1bcc5ebcc9ef51e9b567e568f
brain-dev-null <git@braindevnull.space>
2024-04-23 22:15:49 +0200

    Add evaluation of infix operators between booleans

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -61,8 +61,10 @@ func evalInfixExpression(operator string, left, right object.Object) object.Obje
     switch {
     case left.Type() == object.INTEGER_OBJ && right.Type() == object.INTEGER_OBJ:
         return evalIntegerInfixExpression(operator, left, right)
-    case left.Type() == object.BOOLEAN_OBJ && right.Type() == object.BOOLEAN_OBJ:
-        return evalBooleanInfixExpression(operator, left, right)
+    case operator == "==":
+        return nativeBoolToBooleanObject(left == right)
+    case operator == "!=":
+        return nativeBoolToBooleanObject(left != right)
     default:
         return NULL
     }
@@ -111,13 +113,13 @@ func evalIntegerInfixExpression(operator string, left, right object.Object) obje
     case "/":
         return &object.Integer{Value: leftVal / rightVal}
     case "<":
-        return &object.Boolean{Value: leftVal < rightVal}
+        return nativeBoolToBooleanObject(leftVal < rightVal)
     case ">":
-        return &object.Boolean{Value: leftVal > rightVal}
+        return nativeBoolToBooleanObject(leftVal > rightVal)
     case "==":
-        return &object.Boolean{Value: leftVal == rightVal}
+        return nativeBoolToBooleanObject(leftVal == rightVal)
     case "!=":
-        return &object.Boolean{Value: leftVal != rightVal}
+        return nativeBoolToBooleanObject(leftVal != rightVal)
     default:
         return NULL
     }
