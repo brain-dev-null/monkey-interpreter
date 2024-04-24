58b9dbe47722670793866db05b4e16921f7c6377
brain-dev-null <git@braindevnull.space>
2024-04-24 22:13:19 +0200

    Add evaluation of if-else-expressions

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -31,6 +31,11 @@ func Eval(node ast.Node) object.Object {
         right := Eval(node.Right)
         left  := Eval(node.Left)
         return evalInfixExpression(node.Operator, left, right)
+    case *ast.BlockStatement:
+        return evalStatements(node.Statements)
+
+    case *ast.IfExpression:
+        return evalIfExpression(node)
     }
 
     return nil
@@ -122,6 +127,31 @@ func evalIntegerInfixExpression(operator string, left, right object.Object) obje
         return nativeBoolToBooleanObject(leftVal != rightVal)
     default:
         return NULL
+    }
+}
+
+func evalIfExpression(ie *ast.IfExpression) object.Object {
+    condition := Eval(ie.Condition)
+
+    if isTruthy(condition) {
+        return Eval(ie.Consequence)
+    } else if ie.Alternative != nil {
+        return Eval(ie.Alternative)
+    } else {
+        return NULL
+    }
+}
+
+func isTruthy(obj object.Object) bool {
+    switch obj {
+    case NULL:
+        return false
+    case TRUE:
+        return true
+    case FALSE:
+        return false
+    default:
+        return true
     }
 }
 
