d92c70ba00dbc93f30363be6e3e56e9e5f6c62eb
brain-dev-null <git@braindevnull.space>
2024-04-24 22:38:15 +0200

    Use gofmt

--- object/object.go
+++ object/object.go
@@ -5,33 +5,31 @@ import "fmt"
 type ObjectType string
 
 const (
-    INTEGER_OBJ = "INTEGER"
-    BOOLEAN_OBJ = "BOOLEAN"
-    NULL_OBJ    = "NULL"
+	INTEGER_OBJ = "INTEGER"
+	BOOLEAN_OBJ = "BOOLEAN"
+	NULL_OBJ    = "NULL"
 )
 
 type Object interface {
-    Type() ObjectType
-    Inspect() string
+	Type() ObjectType
+	Inspect() string
 }
 
 type Integer struct {
-    Value int64
+	Value int64
 }
 
 func (i *Integer) Inspect() string  { return fmt.Sprintf("%d", i.Value) }
 func (i *Integer) Type() ObjectType { return INTEGER_OBJ }
 
-
 type Boolean struct {
-    Value bool
+	Value bool
 }
 
 func (b *Boolean) Inspect() string  { return fmt.Sprintf("%t", b.Value) }
 func (b *Boolean) Type() ObjectType { return BOOLEAN_OBJ }
 
-
-type Null struct {}
+type Null struct{}
 
 func (n *Null) Inspect() string  { return "null" }
 func (n *Null) Type() ObjectType { return NULL_OBJ }
